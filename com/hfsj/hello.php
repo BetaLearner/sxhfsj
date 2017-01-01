685c993a9f7f7ac5f90986c24cb836df17488a73
chenzhenhong <chenzhenhong@bytedance.com>
2017-01-01 16:30:40 +0800

    first commit

---
+++ com/hfsj/hello.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenzhenhong
+ * Date: 16/11/12
+ * Time: 下午10:49
+ */
+?>
+<html>
+<head>
+    <title>debug php</title>
+</head>
+<body>
+<p>3+5=</p>
+<?php
+$a = 1;
+$b = 2;
+echo $a + $b;
+?>
+</body>
+</html>
