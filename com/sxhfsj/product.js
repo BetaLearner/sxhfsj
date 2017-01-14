a0236fb97f2004ede495d51708b6e9c05e3f942b
chenzhenhong <chenzhenhong@bytedance.com>
2017-01-14 21:24:31 +0800

    product

--- com/sxhfsj/product.js
+++ com/sxhfsj/product.js
@@ -7,8 +7,8 @@ var weisheng_items = new Array("lianpen.jpg", "tanyu.jpg", "bianpen.jpg", "dabia
 var weisheng_descs = new Array("脸盆", "痰盂", "便盆", "一体便盆", "分体便盆", "尿壶", "女式尿壶", "男式尿壶");
 
 var shuliaobaozhuang_cat = "shuliaobaozhuang";
-var shuliaobaozhuang_items = new Array();
-var shuliaobaozhuang_descs = new Array();
+var shuliaobaozhuang_items = new Array("100ml_ps.jpg", "80_ps.jpg", "160ml_ps.jpg", "450g_ps.jpg", "180pet_ps.jpg", "rhy_ps.jpg", "50_ps.jpg", "60ml_ps.jpg");
+var shuliaobaozhuang_descs = new Array("普通盖塑料瓶", "撕拉盖塑料瓶", "压旋盖塑料瓶", "塑料桶盒", "液体塑料瓶", "液体塑料瓶", "液体塑料瓶", "液体塑料瓶");
 
 var yiliaoqixie_cat = "yiliaoqixie";
 var yiliaoqixie_items = new Array("nijiaotuizhuqi.jpg");
@@ -31,3 +31,13 @@ function get_product_imgs(category, items, descs) {
     document.write(ret);
 }
 
+var objs = new Array("weisheng", "shuliaobaozhuang", "yiliaoqixie", "gongchengsujian")
+function show_product(idx) {
+    for(var i = 0; i < objs.length; ++i) {
+        obj = typeof objs[i] == "string" ? document.getElementById(objs[i]) : objs[i];
+        obj.style.display = "none";
+        if (i==idx) {
+            obj.style.display = "block";
+        }
+    }
+}
