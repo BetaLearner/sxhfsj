97827d66ad967026da7a5f9f3f71168cf0123c15
chenzhenhong <chenzhenhong@bytedance.com>
2017-01-14 17:24:46 +0800

    product, contact

--- com/sxhfsj/product.js
+++ com/sxhfsj/product.js
@@ -2,21 +2,32 @@
  * Created by chenzhenhong on 17/1/8.
  */
 
-function test() {
-    document.write("sxhfsj");
-}
+var weisheng_cat = "weisheng";
+var weisheng_items = new Array("lianpen.jpg", "tanyu.jpg", "bianpen.jpg", "dabianyiti.jpg", "dabianfenti.jpg", "xiaobian.jpg","xiaobiangirl.jpg", "xiaobianboy.jpg");
+var weisheng_descs = new Array("脸盆", "痰盂", "便盆", "一体便盆", "分体便盆", "尿壶", "女式尿壶", "男式尿壶");
 
-var weisheng_items = new Array("dabianyiti.jpg");
-var weisheng_descs = new Array("一体便盆");
-function get_product_imgs(items, descs) {
+var shuliaobaozhuang_cat = "shuliaobaozhuang";
+var shuliaobaozhuang_items = new Array();
+var shuliaobaozhuang_descs = new Array();
+
+var yiliaoqixie_cat = "yiliaoqixie";
+var yiliaoqixie_items = new Array("nijiaotuizhuqi.jpg");
+var yiliaoqixie_descs = new Array("凝胶推注器");
+
+var gongchengsujian_cat = "gongchengsujian";
+var gongchengsujian_items = new Array("gongyesuliaojian.jpg", "suliaobaohumao.jpg");
+var gongchengsujian_descs = new Array("工业塑料件", "塑料保护帽");
+
+function get_product_imgs(category, items, descs) {
     var ret = "";
     for(var i = 0; i < items.length; ++i) {
         ret += "<div class=\"product_img\">"
-            + "<a target=\"_blank\" href=\"source/weisheng/bianpen.jpg\">"
-            + "<img src=\"../../source/" + items[i] + "\" alt=\"Ballade\">"
+            + "<a target=\"_blank\" href=\"../../source/"+category+"/" + items[i] + "\">"
+            + "<img src=\"../../source/" + category + "/" + items[i] + "\" alt=\"Ballade\">"
             + "</a>"
             + "<div class=\"desc\">" + descs[i] + "</div>"
             + "</div>";
     }
     document.write(ret);
 }
+
