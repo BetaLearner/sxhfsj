3f7b0b5f4e02d1f5e9324af8e745a3c5fa2b1f2f
chenzhenhong <chenzhenhong@bytedance.com>
2017-01-15 19:42:59 +0800

    icp

--- com/sxhfsj/frame.js
+++ com/sxhfsj/frame.js
@@ -39,6 +39,6 @@ function header() {
 }
 
 function footer() {
-    var foot_text = "<div class=\"footer\">  版权 © 陕西弘方塑胶有限公司   </div>";
+    var foot_text = "<div class=\"footer\">  版权 © 陕西弘方塑胶有限公司 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 陕ICP备17000599号 </div>";
     document.write(foot_text);
 }
